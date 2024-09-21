9ba3e85713d15488b8d9d1a3f6c2c1a7f04ded89
unknown <manarshblah@gmail.com>
2024-09-21 13:17:25 +0300

    third

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
+import { HashRouter as Router , Route , Routes } from 'react-router-dom';
 import React from 'react';
 import Home from './Home';
 import Login from './component/login';
